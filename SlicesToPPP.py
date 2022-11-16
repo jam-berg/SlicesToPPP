30f9770dde49b04f153c8ef21f7c23cfc7e7a4b0
jam-berg <jamberg@student.ethz.ch>
2022-11-16 12:25:41 +0100

    init commit with powerpoint creation working

---
+++ SlicesToPPP.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import collections.abc
+from pptx import Presentation
+from pptx.dml.color import RGBColor
+from pptx.util import Cm
+
+base_path = '~/ETHZ/Semester 3/Projekt&Praktika III/C8 Radikalische Polymerisation/STL Examples/QuadraTresHelicesRndCrnr/'
+img_path = 'QuadraTresHelicesRndCrnr00120.png'
+
+prs = Presentation()
+slide_masters = prs.slide_masters
+masterfill = slide_masters[0].background.fill
+masterfill.solid()
+masterfill.fore_color.rgb = RGBColor(50, 50, 50)
+title_slide_layout = prs.slide_layouts[9]
+
+for i in range(3):
+	slide = prs.slides.add_slide(title_slide_layout)
+	pic = slide.shapes.add_picture(img_path, Cm(7.34), 0, height=prs.slide_height)
+	slide = prs.slides.add_slide(title_slide_layout)
+
+prs.save('test.pptx')
+
